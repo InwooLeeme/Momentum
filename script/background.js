b37436a955bcd34009ad65909b8b91c23270586e
ItsmeLee <arsmagica90@gmail.com>
2020-07-18 01:09:35 +0900

    Feat : Add ask user name and save in the local Storage
    Ask user name and save it in the local storage
    Change the input style and font size

---
+++ script/background.js
@@ -0,0 +1,7 @@
+var body = document.querySelector('body');
+
+function init(){
+
+}
+
+init();
--- script/sayHello.js
+++ script/sayHello.js
@@ -1,3 +1,45 @@
-var input = document.querySelector('input');
+var hello = document.querySelector('.js-sayHello'),
+    form = document.querySelector('.input__form'),
+    input = form.querySelector('.askUserName');
 
-console.log(input);
+var User_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(User_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    var currentValue = input.value;
+    paintName(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit);
+    
+}
+
+function paintName(text){
+    form.classList.remove(SHOWING_CN);
+    hello.classList.add(SHOWING_CN);
+    hello.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    var currentUser = localStorage.getItem(User_LS);
+    if(currentUser === null){
+        askForName();
+    }
+    else{
+        paintName(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
