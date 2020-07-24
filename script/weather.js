4a139c70e18cf15c1ce21a0adbc70db99cc9f232
ItsmeLee <arsmagica90@gmail.com>
2020-07-24 17:55:13 +0900

    feat : Add the weather function
    Add the weather function using open weather map API
    And css header section

--- script/weather.js
+++ script/weather.js
@@ -1,5 +1,29 @@
 
+var weather = document.querySelector('.js-weather'),
+    locate = document.querySelector('.js-place'),
+    weatherIcon = document.querySelector('#weather-icon');
+var API_KEY = "bb407ffd7cb86483d91e304b88d5db02";
 var COORDS = 'coords';
+
+function getWeather(lat, lon){
+fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        var temperature = json.main.temp,
+            place = json.name,
+            icon = json.weather[0].main;
+        weather.innerText = `${temperature}`;
+        locate.innerText = `${place}`;
+        if(icon === 'Clouds'){
+            weatherIcon.className += "fas fa-cloud fa-2x";
+        }
+        /* console.log(json.weather[0].main);
+        console.log(weatherIcon); */
+    });
+}
+
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -14,6 +38,7 @@ function handleGeoSuccess(poistion){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -32,7 +57,8 @@ function loadCoords(){
         askForCoords();
     }
     else{
-        getWeather();
+        var parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 
 }
